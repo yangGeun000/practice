2dd4bff508f0866970150c163bda99418dd255fa
green <keylink000@gmail.com>
2022-10-05 11:13:41 +0900

    불필요한 프로젝트 삭제

--- back/spring/demo/src/main/java/com/test/demo/question/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.demo.question;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer>{
-	Question findBySubject(String subject);
-}
